fd72935d0fdf3bb257b7dbc86af8bab7540bf8d7
Saadiq Hamza <hmsaadiq@gmail.com>
2026-02-01 15:15:18 +0300

    removed unnecessary files

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// POSTCSS CONFIG: This file configures PostCSS for the frontend build process.
-// It enables Tailwind CSS and autoprefixer for CSS processing.
-
-module.exports = {
-  plugins: {
-    tailwindcss: {}, // Enable Tailwind CSS.
-    autoprefixer: {}, // Enable autoprefixer for vendor prefixes.
-  },
-} 
